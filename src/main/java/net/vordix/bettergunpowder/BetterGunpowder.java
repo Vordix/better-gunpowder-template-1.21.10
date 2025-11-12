9330089948f56ec733896691a75ec6c1e7701b72
Vordix <philippvorderbrueggen@gmail.com>
2025-11-12 19:46:18 +0100

    added first important comment for following the tutorial by kaupenjoe reasons

--- src/main/java/net/vordix/bettergunpowder/BetterGunpowder.java
+++ src/main/java/net/vordix/bettergunpowder/BetterGunpowder.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class BetterGunpowder implements ModInitializer {
 	public static final String MOD_ID = "bettergunpowder";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+// Very Very Important Comment for following the tutorial reasons
 	@Override
 	public void onInitialize() {
 
