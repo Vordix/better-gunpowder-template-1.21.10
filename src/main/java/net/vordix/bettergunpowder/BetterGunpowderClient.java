f3b74a5a7eb60f7a855b839c0be0503555d3d55f
Vordix <philippvorderbrueggen@gmail.com>
2025-11-12 19:44:16 +0100

    Initial commit

---
+++ src/main/java/net/vordix/bettergunpowder/BetterGunpowderClient.java
@@ -0,0 +1,10 @@
+package net.vordix.bettergunpowder;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterGunpowderClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/vordix/bettergunpowder/BetterGunpowderDataGenerator.java
@@ -0,0 +1,11 @@
+package net.vordix.bettergunpowder;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterGunpowderDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
